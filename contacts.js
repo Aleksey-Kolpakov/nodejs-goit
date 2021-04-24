961f314ffa86f5e8ffb9172e16f5272e1f0906e9
Aleksey Kolpakov <gmsaleksei@gmail.com>
2021-04-24 18:55:05 +0300

    fin

--- contacts.js
+++ contacts.js
@@ -1,27 +1,59 @@
-import path from "path";
-import fs from "fs";
-const contactsPath = path.extname('contacts.json');
-console.log(contactsPath);
+const fs = require("fs");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+const fsPromises = fs.promises;
+const contactsPath = path.join(__dirname, "/db/contacts.json");
 
-function listContacts() {
-  // ...твой код
+async function getlistContacts() {
+  try {
+    const listContacts = await fsPromises.readFile(contactsPath, "utf-8");
+    return JSON.parse(listContacts);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-function getContactById(contactId) {
-  // ...твой код
+async function getContactById(contactId) {
+  try {
+    const contactList = await getlistContacts();
+    const contact = contactList.find(contact => String(contact.id) === String(contactId));
+    return contact;
+  } catch (error) {
+    console.log(error);
+  }
 }
-
-function removeContact(contactId) {
-  // ...твой код
+async function removeContact(contactId) {
+  try {
+    const contactList = await getlistContacts();
+    const updatedContactList = contactList.filter(
+      (contact) => String(contact.id) !== String(contactId)
+    );
+    console.log(updatedContactList);
+    fs.writeFile(contactsPath, JSON.stringify(updatedContactList), (err) => {
+      if (err) throw err;
+      console.log("Write file finished successfully");
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
-
-function addContact(name, email, phone) {
-  // ...твой код
+async function addContact(name, email, phone) {
+  try {
+    const contactList = await getlistContacts();
+    const newContact = { id: uuidv4(), name, email, phone };
+    const updatedContactList = [...contactList, newContact];
+    console.log(updatedContactList);
+    fs.writeFile(contactsPath, JSON.stringify(updatedContactList), (err) => {
+      if (err) throw err;
+      console.log("Write file finished successfully");
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
-
-export default {
-    listContacts,
-    getContactById,
-    removeContact,
-    addContact,
-}
+module.exports = {
+  getlistContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
